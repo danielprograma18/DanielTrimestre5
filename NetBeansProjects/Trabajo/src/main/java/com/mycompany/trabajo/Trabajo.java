7bb0b242eff82cb60bf52890a45b5739006881a1
Daniel cardenas <df028231@gmail.com>
2024-12-07 18:16:02 -0500

    Netbnans Bases de datos

---
+++ NetBeansProjects/Trabajo/src/main/java/com/mycompany/trabajo/Trabajo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.trabajo;
+
+/**
+ *
+ * @author Janus
+ */
+public class Trabajo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
